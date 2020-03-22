e65f24c520d1d96d383c40735f48bedfdd062450
parapore <eriglai577@gmail.com>
2020-03-22 12:47:47 +0900

    basic認証

---
+++ src/auth/AuthTest1.java
@@ -0,0 +1,33 @@
+package auth;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/AuthTest1")
+public class AuthTest1 extends HttpServlet {
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+        throws IOException, ServletException{
+
+        response.setContentType("text/html; charset=Shift_JIS");
+        PrintWriter out = response.getWriter();
+
+        out.println("<html>");
+        out.println("<head>");
+        out.println("<title>ユーザー認証テスト</title>");
+        out.println("</head>");
+        out.println("<body>");
+
+        out.println("<p>テストページ1</p>");
+
+        out.println("<p><a href=\"/logintest/AuthTest2\">テストページ2へ</a></p>");
+
+        out.println("</body>");
+        out.println("</html>");
+    }
+}
---
+++ src/auth/AuthTest2.java
@@ -0,0 +1,33 @@
+package auth;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/AuthTest2")
+public class AuthTest2 extends HttpServlet {
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+        throws IOException, ServletException{
+
+        response.setContentType("text/html; charset=Shift_JIS");
+        PrintWriter out = response.getWriter();
+
+        out.println("<html>");
+        out.println("<head>");
+        out.println("<title>ユーザー認証テスト</title>");
+        out.println("</head>");
+        out.println("<body>");
+
+        out.println("<p>テストページ2</p>");
+
+        out.println("<p><a href=\"/logintest/AuthTest1\">テストページ1へ</a></p>");
+
+        out.println("</body>");
+        out.println("</html>");
+    }
+}
